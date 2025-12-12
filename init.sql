c86a7c82ee7022e9e60db3dd045a4317c75b5c0d
tang <natnanon.l66@rsu.ac.th>
2025-12-13 01:04:39 +0700

    5

--- init.sql
+++ init.sql
@@ -1,10 +1,11 @@
 CREATE TABLE IF NOT EXISTS todo (
   id INT AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(255) NOT NULL,
-  completed TINYINT(1) DEFAULT 0
+  completed TINYINT(1) DEFAULT 0,
+  event_datetime DATETIME NULL
 );
 
-INSERT INTO todo (title, completed) VALUES
-('อ่านหนังสือสอบ', 0),
-('ทำการบ้าน', 0),
-('ออกกำลังกาย', 1);
+INSERT INTO todo (title, completed, event_datetime) VALUES
+('อ่านหนังสือสอบ', 0, '2025-12-13 09:00:00'),
+('ทำการบ้าน', 0, '2025-12-13 14:00:00'),
+('ออกกำลังกาย', 1, '2025-12-13 18:00:00');
